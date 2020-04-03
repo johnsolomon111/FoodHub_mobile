257d2f40c733cf33c16696b10f6ad122e85e0087
unknown <johnbenedict.solomon@g.msuiit.edu.ph>
2020-04-03 18:34:56 +0800

    Added Privacy, Deactivate, Logout with Alerts

--- src/app/tab1/settings/settings.page.ts
+++ src/app/tab1/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -7,8 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
+  async logoutAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirm Action',
+      message: 'Are you sure you want to logout?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('logout');
+          }
+        }, {
+          text: 'Logout',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  async deactivateAlert() {
+    const alert = await this.alertController.create({
+      header: 'Deactivate Account',
+      message: 'Your account will be deactivated and other users can not view your profile',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('logout');
+          }
+        }, {
+          text: 'Confirm',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  async privacyAlert() {
+    const alert = await this.alertController.create({
+      header: 'Private Account',
+      message: 'Your account is hidden for other users',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
   ngOnInit() {
   }
 
